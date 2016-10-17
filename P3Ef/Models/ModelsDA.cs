da0bfe9d8d15ad65053c2c31383b567504dffa56
OlegGolovchenko <igrok_be@hotmail.com>
2016-10-17 16:31:32 +0200

    views adaptation

--- P3Ef/Models/ModelsDA.cs
+++ P3Ef/Models/ModelsDA.cs
@@ -32,6 +32,8 @@ namespace Models
             return Sources.Where(s => s.PName == pname).ToList();
         }
 
+        public System.Data.Entity.DbSet<Models.Language> Languages { get; set; }
+
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
